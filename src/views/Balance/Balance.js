2e374b8fc418744f5cb862fec202dab5bdfa9817
osereda <osereda.dev@gmail.com>
2021-02-27 22:10:54 +0200

    added new logic

--- src/views/Balance/Balance.js
+++ src/views/Balance/Balance.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Edit from "@material-ui/icons/Edit";
-import Close from "@material-ui/icons/Close";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Table from "components/Table/Table.js";
@@ -11,17 +9,12 @@ import CardBody from "components/Card/CardBody.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardHeader from "components/Card/CardHeader.js";
 import styles from "assets/jss/material-dashboard-pro-react/views/extendedTablesStyle.js";
-import BatteryChargingFullIcon from '@material-ui/icons/BatteryChargingFull';
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import CustomDropdown from "../../components/CustomDropdown/CustomDropdown";
 import configData from "../../config.json";
 import Grid from "@material-ui/core/Grid";
-import {NativeSelect} from "@material-ui/core";
 import {KeyboardDatePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 import EuroIcon from '@material-ui/icons/Euro';
-import isWeekend from 'date-fns/isWeekend';
+
 
 const useStyles = makeStyles(styles);
 
@@ -29,8 +22,7 @@ function BalanceTable(inputData) {
 
     const classes = useStyles();
     const balance = inputData.inputDataBalance;
-    const tableHeadData=["#", "Date", "Power, kW",  "Time, h", "Price, $", "Actions"]
-    const [selectCountry, setSelectCountry] = React.useState("");
+    const tableHeadData=["#", "Date", "Power, kW",  "Time, h", "Price, $"]
     const [selectCity, setSelectCity] = React.useState("");
     const [selectedFilter, setFilter] = React.useState(1);
     const [selectedFilterStat, setFilterSat] = React.useState('Last month');
@@ -62,23 +54,10 @@ function BalanceTable(inputData) {
         }
     };
 
-    const fillButtons = [
-        { color: "info", icon: BatteryChargingFullIcon },
-        { color: "success", icon: Edit },
-        { color: "danger", icon: Close }
-    ].map((prop, key) => {
-        return (
-            <Button color={prop.color} className={classes.actionButton} key={key} disabled>
-                <prop.icon className={classes.icon} />
-            </Button>
-        );
-    });
-
     let filteredDates = [];
     balance.forEach(balance => {
         let arrInner = [];
         arrInner = balance.slice();
-        arrInner.push(fillButtons);
         FilteredData.push(arrInner);
         filteredDates = FilteredData.filter(date => {
             if(new Date(date[1]) - new Date(selectedDateFrom.toString()) >= 0 &&
@@ -215,21 +194,15 @@ function BalanceTable(inputData) {
                                 classes.center,
                                 classes.center,
                                 classes.center,
-                                classes.center,
-                                classes.center,
-                                classes.center,
-                                classes.right]}
-                            customClassesForCells={[0,1,2,3,4,5,6,7]}
+                                classes.center]}
+                            customClassesForCells={[0,1,2,3,4]}
                             customHeadCellClasses={[
                                 classes.left,
                                 classes.center,
                                 classes.center,
                                 classes.center,
-                                classes.center,
-                                classes.center,
-                                classes.center,
-                                classes.right]}
-                            customHeadClassesForCells={[0,1,2,3,4,5,6,7]}
+                                classes.center]}
+                            customHeadClassesForCells={[0,1,2,3,4]}
                         />
                     </CardBody>
                 </Card>
@@ -271,8 +244,17 @@ export default class Balance extends React.Component {
             item._id = ++i;
             item.bl_date = new Date(item.bl_date).toLocaleDateString("en-US") ;
             item.bl_pow = item.bl_pow ? item.bl_pow.toFixed(2) : 0;
-            item.bl_price = item.bl_price ? item.bl_price.toFixed(2) : 0;
-            item.bl_time = item.bl_time ? item.bl_time.toFixed(2) : 0;
+            item.bl_price = item.bl_price ? item.bl_price.toFixed(2)*(-1) : 0;
+            if(item.bl_time) {
+                if(item.bl_time/3600 > 1){
+                    let hour = (item.bl_time/3600).toFixed(0);
+                    let min = ((item.bl_time%3600)/60).toFixed(0);
+                    item.bl_time = min >= 10 ? hour + " : " + min : hour + " : 0" + min;
+                } else {
+                    let min = (item.bl_time/60).toFixed(0);
+                    item.bl_time = min >= 10  ? "0 : " + min : "0 : 0" + min;
+                }
+            }
             delete item.bl_location;
             delete item.bl_scooter_event;
             outData.push(Object.values(item));
